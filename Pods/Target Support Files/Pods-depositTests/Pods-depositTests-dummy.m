86454565069071fb708a904fd0b5dc1860fe0f7a
daiki <daimatsu1230@yahoo.co.jp>
2017-05-29 12:22:50 +0900

    master commit

---
+++ Pods/Target Support Files/Pods-depositTests/Pods-depositTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_depositTests : NSObject
+@end
+@implementation PodsDummy_Pods_depositTests
+@end
---
+++ deposit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  deposit
+//
+//  Created by matsuuradaiki on 2017/05/23.
+//  Copyright © 2017年 matsuuradaiki. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
